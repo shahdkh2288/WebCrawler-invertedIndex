53c7cf4771b3b3e43390099f98abe1812d514838
shahdkh2288 <101909725+shahdkh2288@users.noreply.github.com>
2024-05-18 14:19:44 +0300

    Updated WebCrawler Class

---
+++ is322_HW_3/src/crawler/WebCrawlerWithDepth.java
@@ -0,0 +1,178 @@
+package crawler;
+
+/**
+ *
+ * @author ehab
+ */
+
+import invertedIndex.SourceRecord;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Map;
+//==============================================================================
+
+public class WebCrawlerWithDepth {
+
+    private static final int MAX_DEPTH =2;
+    private static final int MAX_PER_PAGE = 6;
+    int max_docs = 20;
+    private HashSet<String> links;
+    Map<Integer, SourceRecord> sources;
+    //   Map<Integer, String> doc_text;
+    int fid = 0;
+    int plinks = 0;
+    //String storageName;
+//==============================================================================
+
+    public WebCrawlerWithDepth() {
+        links = new HashSet<>();
+        sources = null;
+        fid = 0;
+    }
+
+    public WebCrawlerWithDepth(invertedIndex.Index5 in) {
+        links = new HashSet<>();
+        sources = in.sources;
+        fid = 0;
+    }
+
+    public void setSources(invertedIndex.Index5 in) {
+        sources = in.sources;
+    }
+//==============================================================================
+
+    public String getText(Document document) {
+        String pAcc = "";
+        Elements p = document.body().getElementsByTag("p");
+
+        for (Element e : p) {
+            pAcc += e.text();
+        }
+        return pAcc;
+    }
+//==============================================================================
+
+    public void getPageLinks(String URL, int depth, invertedIndex.Index5 index) {
+       System.out.println("|| URL: [" + URL + "] --------  depth: " + depth + " fid=" + fid + " plinks=" + plinks + "\t|||| ");
+
+        if ((!(links.contains(URL)))
+                && (depth < MAX_DEPTH)
+                && (fid < max_docs)
+                //        && ((depth == 1) || (plinks < (MAX_PER_PAGE * (depth + 1))))
+                && ((depth == 0)
+                || ((depth == 1) && (plinks < ((MAX_PER_PAGE) + 290)))
+                || (plinks < ((MAX_PER_PAGE * (depth + 1)) - (plinks / 2))))
+                && (!URL.contains("https://.m."))
+                && (URL.contains("https://en.w"))
+                && (!URL.contains("wiki/Wikipedia"))
+                && (!URL.contains("searchInput"))
+                && (!URL.contains("wiktionary"))
+                && (!URL.contains("#"))
+                && (!URL.contains(","))
+                && (!URL.contains("Wikiquote"))
+                && (!URL.contains("disambiguation"))
+                && (!URL.contains("w/index.php"))
+                && (!URL.contains("wikimedia"))
+                && (!URL.contains("/Privacy_policy"))
+                && (!URL.contains("Geographic_coordinate_system"))
+                && (!URL.contains(".org/licenses/"))
+                && ((!URL.substring(12).contains(":")) || (depth == 0)) // ignor sublink that contain : bu pass the "http:"
+                && (!URL.isEmpty())
+                && (!URL.contains("Main_Page"))
+                && (!URL.contains("mw-head"))) {
+            try {
+                // Step 1: Add this URL to the visited list
+                links.add(URL);
+
+                // Step 2: Initialize the document element using the Jsoup library
+                Document document = Jsoup.connect(URL).get();
+
+                // Step 3: Get all links of the page
+                Elements linksOnPage = document.select("a[href]");
+
+                // Step 4: Get all paragraphs <p> elements from the page
+                String docText = getText(document);
+
+                // Step 5: Build the sources
+                SourceRecord sr = new SourceRecord(fid, URL, document.title(), docText.substring(0, Math.min(30, docText.length())));
+                sr.length = docText.length();
+                sources.put(fid, sr);
+
+                // Pass the docText for the inverted index with the doc id
+                index.addDocument(fid, docText);
+
+                plinks++;  // accumulator for the link in a sub-branch
+                fid++;   // current document id
+
+                // Step 6: Handle all the page hyperlinks "linksOnPage" recursively with depth +1
+                for (Element page : linksOnPage) {
+                    getPageLinks(page.attr("abs:href"), depth + 1, index);
+                }
+                plinks--;
+            } catch (IOException e) {
+                System.err.println("For '" + URL + "': " + e.getMessage());
+            }
+        }
+    }
+//==============================================================================
+
+    public void parsePageLinks(String URL, int depth, invertedIndex.Index5 index) {
+        System.out.println("--------------- URL: " + URL + " --------  depth: " + depth + " - - - - - - --------- ");
+
+        plinks = 0;
+        getPageLinks(URL, depth, index);
+    }
+//==============================================================================
+
+    public String getSourceName(int id) {
+        return sources.get(id).getURL();
+    }
+//==============================================================================
+
+    void printSources() {
+        for (int i = 0; i < sources.size(); i++) {
+            System.out.println(">>  " + i + " [" + getSourceName(i) + "]");
+        }
+    }
+
+    public invertedIndex.Index5 initializeNew(String storageName) {
+        invertedIndex.Index5 index = new invertedIndex.Index5();
+        setSources(index);
+        index.createStore(storageName);
+        return index;
+    }
+
+    public invertedIndex.Index5 initialize(String storageName) {
+        invertedIndex.Index5 index = new invertedIndex.Index5();
+        setSources(index);
+        setDomainKnowledge(index, storageName);
+        index.setN(fid);
+        index.store(storageName);
+
+        return index;
+    }
+
+    void setDomainKnowledge(invertedIndex.Index5 index, String domain) {
+        if (domain.equals("test")) {
+            parsePageLinks("https://en.wikipedia.org/wiki/List_of_pharaohs", 0, index);
+            parsePageLinks("https://en.wikipedia.org/wiki/Cairo", 0, index);
+
+        }
+
+    }
+
+//==============================================================================
+    public static void main(String[] args) {
+
+        WebCrawlerWithDepth wc = new WebCrawlerWithDepth();
+
+        invertedIndex.Index5 index = wc.initialize("test"); //   ukraine
+            index.find_07a("narmer giza pyramid");
+            index.searchLoop();
+    }
+}
---
+++ is322_HW_3/src/invertedIndex/Index5.java
@@ -0,0 +1,362 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package invertedIndex;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import static java.lang.Math.log10;
+import static java.lang.Math.sqrt;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author ehab
+ */
+public class Index5 {
+
+    //--------------------------------------------
+    int N = 0;
+    public Map<Integer, SourceRecord> sources;  // store the doc_id and the file name.
+
+    public HashMap<String, DictEntry> index; // THe inverted index
+    //--------------------------------------------
+
+    SortedScore sortedScore;
+    //--------------------------------------------
+    public Index5() {
+        sources = new HashMap<Integer, SourceRecord>();
+        index = new HashMap<String, DictEntry>();
+    }
+
+    public void setN(int n) {
+        N = n;
+    }
+
+
+    //---------------------------------------------
+    public void printPostingList(Posting p) {
+        // Iterator<Integer> it2 = hset.iterator();
+        System.out.print("[");
+        while (p != null) {
+            /// -4- **** complete here ****
+            // fix get rid of the last comma
+            System.out.print("" + p.docId + "," );
+            p = p.next;
+        }
+        System.out.println("]");
+    }
+
+    //---------------------------------------------
+    public void printDictionary() {
+        Iterator it = index.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            DictEntry dd = (DictEntry) pair.getValue();
+            System.out.print("** [" + pair.getKey() + "," + dd.doc_freq + "]       =--> ");
+            printPostingList(dd.pList);
+        }
+        System.out.println("------------------------------------------------------");
+        System.out.println("*** Number of terms = " + index.size());
+    }
+     //----------------------------------------------------------------------------  
+    public int buildIndex(String ln, int fid) {
+        int flen = 0;
+
+        String[] words = ln.split("\\W+");
+        //  String[] words = ln.replaceAll("(?:[^a-zA-Z0-9 -]|(?<=\\w)-(?!\\S))", " ").toLowerCase().split("\\s+");
+        flen += words.length;
+        for (String word : words) {
+            word = word.toLowerCase();
+            if (stopWord(word)) {
+                continue;
+            }
+            word = stemWord(word);
+            // check to see if the word is not in the dictionary
+            if (!index.containsKey(word)) {
+                index.put(word, new DictEntry());
+            }
+            // add document id to the posting list
+            if (!index.get(word).postingListContains(fid)) {
+                index.get(word).doc_freq += 1; //set doc freq to the number of doc that contain the term 
+                if (index.get(word).pList == null) {
+                    index.get(word).pList = new Posting(fid);
+                    index.get(word).last = index.get(word).pList;
+                } else {
+                    index.get(word).last.next = new Posting(fid);
+                    index.get(word).last = index.get(word).last.next;
+                }
+            } else {
+                index.get(word).last.dtf += 1;
+            }
+            //set the term_fteq in the collection
+            index.get(word).term_freq += 1;
+            if (word.equalsIgnoreCase("lattice")) {
+
+                System.out.println("  <<" + index.get(word).getPosting(1) + ">> " + ln);
+            }
+
+        }
+        return flen;
+    }
+
+    //-----------------------------------------------
+    public void buildIndex(String[] files) {  // from disk not from the internet
+        int fid = 0;
+        for (String fileName : files) {
+            try (BufferedReader file = new BufferedReader(new FileReader(fileName))) {
+                if (!sources.containsKey(fileName)) {
+                    sources.put(fid, new SourceRecord(fid, fileName, fileName, "notext"));
+                }
+                String ln;
+                int flen = 0;
+                while ((ln = file.readLine()) != null) {
+                    /// -2- **** complete here ****
+                    ///**** hint   flen +=  ________________(ln, fid);
+                }
+                sources.get(fid).length = flen;
+
+            } catch (IOException e) {
+                System.out.println("File " + fileName + " not found. Skip it");
+            }
+            fid++;
+        }
+        //   printDictionary();
+    }
+
+    //----------------------------------------------------------------------------  
+    public int indexOneLine(String ln, int fid) {
+        int flen = 0;
+
+        String[] words = ln.split("\\W+");
+      //   String[] words = ln.replaceAll("(?:[^a-zA-Z0-9 -]|(?<=\\w)-(?!\\S))", " ").toLowerCase().split("\\s+");
+        flen += words.length;
+        for (String word : words) {
+            word = word.toLowerCase();
+            if (stopWord(word)) {
+                continue;
+            }
+            word = stemWord(word);
+            // check to see if the word is not in the dictionary
+            // if not add it
+            if (!index.containsKey(word)) {
+                index.put(word, new DictEntry());
+            }
+            // add document id to the posting list
+            if (!index.get(word).postingListContains(fid)) {
+                index.get(word).doc_freq += 1; //set doc freq to the number of doc that contain the term 
+                if (index.get(word).pList == null) {
+                    index.get(word).pList = new Posting(fid);
+                    index.get(word).last = index.get(word).pList;
+                } else {
+                    index.get(word).last.next = new Posting(fid);
+                    index.get(word).last = index.get(word).last.next;
+                }
+            } else {
+                index.get(word).last.dtf += 1;
+            }
+            //set the term_fteq in the collection
+            index.get(word).term_freq += 1;
+            if (word.equalsIgnoreCase("lattice")) {
+
+                System.out.println("  <<" + index.get(word).getPosting(1) + ">> " + ln);
+            }
+
+        }
+        return flen;
+    }
+
+//----------------------------------------------------------------------------  
+    boolean stopWord(String word) {
+        if (word.equals("the") || word.equals("to") || word.equals("be") || word.equals("for") || word.equals("from") || word.equals("in")
+                || word.equals("a") || word.equals("into") || word.equals("by") || word.equals("or") || word.equals("and") || word.equals("that")) {
+            return true;
+        }
+        if (word.length() < 2) {
+            return true;
+        }
+        return false;
+
+    }
+//----------------------------------------------------------------------------  
+
+    String stemWord(String word) { //skip for now
+        return word;
+//        Stemmer s = new Stemmer();
+//        s.addString(word);
+//        s.stem();
+//        return s.toString();
+    }
+
+    //----------------------------------------------------------------------------  
+    Posting intersect(Posting pL1, Posting pL2) {
+///****  -1-   complete after each comment ****
+//   INTERSECT ( p1 , p2 )
+//          1  answer ←      {}
+        Posting answer = null;
+        Posting last = null;
+//      2 while p1  != NIL and p2  != NIL
+     
+//          3 do if docID ( p 1 ) = docID ( p2 )
+ 
+//          4   then ADD ( answer, docID ( p1 ))
+                // answer.add(pL1.docId);
+ 
+//          5       p1 ← next ( p1 )
+//          6       p2 ← next ( p2 )
+ 
+ //          7   else if docID ( p1 ) < docID ( p2 )
+            
+//          8        then p1 ← next ( p1 )
+//          9        else p2 ← next ( p2 )
+ 
+//      10 return answer
+        return answer;
+    }
+
+
+    //---------------------------------
+    String[] sort(String[] words) {  //bubble sort
+        boolean sorted = false;
+        String sTmp;
+        //-------------------------------------------------------
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < words.length - 1; i++) {
+                int compare = words[i].compareTo(words[i + 1]);
+                if (compare > 0) {
+                    sTmp = words[i];
+                    words[i] = words[i + 1];
+                    words[i + 1] = sTmp;
+                    sorted = false;
+                }
+            }
+        }
+        return words;
+    }
+//==========================================================    
+   public String find_07a(String phrase) {
+        System.out.println("-------------------------  find_07 -------------------------");
+
+        String result = "";
+        String[] words = phrase.split("\\W+");
+        int len = words.length;
+        sortedScore = new SortedScore();
+
+        double scores[] = new double[N];
+        double qwt[] = new double[len];
+        double qnz[] = new double[len];
+
+//1 float Scores[N] = 0
+//2 Initialize Length[N]
+//3 for each query term t
+//for (String term : terms) {
+//4 do calculate w t, q and fetch postings list for t
+//term = term.toLowerCase();
+//int tdf = index.get(term).doc_freq; // number of documents that contains the term
+//int ttf = index.get(term).term_freq; //
+//4.a compute idf
+//idf = log10(N / (double) tdf); // can be computed earlier
+//5 for each pair(doc_id, dtf ) in postings list
+//6 add the term score for (term/doc) to score of each doc
+//scores[p.docId] += (1 + log10((double) p.dtf)) * idf);
+//Normalize for the length of the doc
+//7 Read the array Length[d]
+//8 for each d
+//9 do Scores[d] = Scores[d]/Length[d]
+//10 return Top K components of Scores[]
+
+        return result;
+    }
+/////---------------------------------
+public void searchLoop() {
+
+        String phrase;
+        do {
+            System.out.println("Print search phrase: ");
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                phrase = in.readLine();
+                find_07a(phrase);
+                //        find_08(phrase);
+            } catch (Exception e) {
+                e.printStackTrace();
+                break;
+            }
+        } while (!phrase.isEmpty());
+
+    }
+     //---------------------------------
+
+    public void store(String storageName) {
+        try {
+            String pathToStorage = "/home/ehab/tmp11/rl/"+storageName;
+            Writer wr = new FileWriter(pathToStorage);
+            for (Map.Entry<Integer, SourceRecord> entry : sources.entrySet()) {
+                System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue().URL + ", Value = " + entry.getValue().title + ", Value = " + entry.getValue().text);
+                wr.write(entry.getKey().toString() + ",");
+                wr.write(entry.getValue().URL.toString() + ",");
+                wr.write(entry.getValue().title.replace(',', '~') + ",");
+                wr.write(entry.getValue().length + ","); //String formattedDouble = String.format("%.2f", fee );
+                wr.write(String.format("%4.4f", entry.getValue().norm) + ",");
+                wr.write(entry.getValue().text.toString().replace(',', '~') + "\n");
+            }
+            wr.write("section2" + "\n");
+
+            Iterator it = index.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry) it.next();
+                DictEntry dd = (DictEntry) pair.getValue();
+                //  System.out.print("** [" + pair.getKey() + "," + dd.doc_freq + "] <" + dd.term_freq + "> =--> ");
+                wr.write(pair.getKey().toString() + "," + dd.doc_freq + "," + dd.term_freq + ";");
+                Posting p = dd.pList;
+                while (p != null) {
+                    //    System.out.print( p.docId + "," + p.dtf + ":");
+                    wr.write(p.docId + "," + p.dtf + ":");
+                    p = p.next;
+                }
+                wr.write("\n");
+            }
+            wr.write("end" + "\n");
+            wr.close();
+            System.out.println("=============EBD STORE=============");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+//=========================================    
+    public boolean storageFileExists(String storageName){
+        java.io.File f = new java.io.File("/home/ehab/tmp11/rl/"+storageName);
+        if (f.exists() && !f.isDirectory())
+            return true;
+        return false;
+            
+    }
+//----------------------------------------------------    
+    public void createStore(String storageName) {
+        try {
+            String pathToStorage = "/home/ehab/tmp11/"+storageName;
+            Writer wr = new FileWriter(pathToStorage);
+            wr.write("end" + "\n");
+            wr.close();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addDocument(int fid, String docText) {
+    }
+}
+
+//=====================================================================
